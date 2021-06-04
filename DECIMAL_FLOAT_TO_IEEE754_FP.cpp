a980a43bd15f70f85e1276149d45496c175beb78
ksaifahmed <ksaifahmed@gmail.com>
2021-06-04 12:16:07 +0600

    markdown added

--- DECIMAL_FLOAT_TO_IEEE754_FP.cpp
+++ DECIMAL_FLOAT_TO_IEEE754_FP.cpp
@@ -17,6 +17,7 @@ using namespace std;
 //decimal places
 #define PRECISION 5
 
+//converts a decimal int to binary
 string binar(int dig)
 {
     string bin = ""; int rem;
@@ -29,24 +30,41 @@ string binar(int dig)
     return bin;
 }
 
+//converts a decimal float to binary string
+//eg. 4.25 to 100.01
 tuple<string, string, string> f_to_b(float f)
 {
+	//finding sign of input f
     int sgn = 0;
     if(abs(f)!=f && f!=0.0) sgn = 1;
     string sign = to_string(sgn);
+	
+	///get binary of the int part of the float
+	//if 4.25, this would get binary of 4 only
     string bin = binar(abs(int(f)));
 
+	//split into two using the dot, take the part after the point
+	//eg 0.25 in 4.25
     string d_str = to_string(abs(f));
     if(d_str.find(".")<d_str.length()) d_str = d_str.substr(d_str.find("."));
-    if(d_str.length()-1>PRECISION) d_str = d_str.substr(0, PRECISION+1);
+    
+	//takes dp upto precision
+	if(d_str.length()-1>PRECISION) d_str = d_str.substr(0, PRECISION+1);
+	
+	//converts 0.25 back to float for calculations
     float a_dec = stof(d_str); d_str = "";
 
-    while(!(a_dec<(1.0/pow(10, PRECISION))))
+	//0.25 to binary
+    while(!(a_dec<(1.0/pow(10, PRECISION)))) ///while a is not small enough (can't be perfectly zero)
     {
+		//appending ans to empty string d_str
         a_dec *= 2;
         d_str.append(to_string(int(a_dec)));
         a_dec = a_dec - float(int(a_dec));
     }
+	
+	//return 4.25's binary 100.01
+	//like make_tuple(100, 01, 0);
     return make_tuple(bin, d_str, sign);
 }
 
@@ -54,6 +72,8 @@ tuple<string, string, string> f_to_b(float f)
 tuple<string, int> pop_zeroes(string bin_l)
 {
     int i = 11, expbase = 0;
+	
+	//while zeroes, remove em and decrease exponent
     while(i--)
     {
         if(bin_l.at(0) == '0')
@@ -70,22 +90,31 @@ tuple<string, int> pop_zeroes(string bin_l)
     return make_tuple(bin_l, (expbase+BIAS));
 }
 
+
+//binary float eg 100.01 to IEEE standard
+///bin_f is part before binary point, bin_l is after, sgn is sign
 string standardize(string bin_f, string bin_l, string sgn)
 {
     string bin = ""; int exp;
+	
+	//eg if input == 100.01, i.e. bin_f = 100
     if(bin_f.length()>1)
     {
         exp = bin_f.length()-1+BIAS;
         bin = bin_f.substr(1) + bin_l;
         if(bin.length()>11) bin = bin.substr(0, 11);
-        return sgn+" "+binar(exp)+" "+bin;
+        return sgn+" "+binar(exp)+" "+bin; //bin is bin_f+bin_l
     }
 
+	//eg if input == 0.01, i.e. bin_f is null
     if(bin_f.empty())
     {
-        tie(bin_l, exp) = pop_zeroes(bin_l);
+        tie(bin_l, exp) = pop_zeroes(bin_l); //removes the zeroes and decreases exponent
         return sgn+" "+binar(exp)+" "+bin_l;
     }
+	
+	//else exp is zero, bin_f.len = 1
+	//eg binary of 1.5 aka 1.1
     return sgn + " " + binar(BIAS) + " " +bin_l;
 }
 
---
+++ README.md
@@ -0,0 +1,31 @@
+1) ##### **DECIMAL_FLOAT_TO_IEEE754_FP.cpp**
+	
+	> converts decimal float to IEEE754 format 16-bit floating point:
+		 1-bit sign | 4-bit exponent | 11-bit fraction/mantissa
+	
+	> in-line comments added as a doc
+	
+
+
+2) ##### **FPA_TC_GEN.cpp**
+	
+	> same as above, but generates random floats between macros HI and LO
+	
+	> check main()
+	
+
+
+3) ##### **FPA_TC_GEN_JSON.cpp**
+
+	> generates random floats and prints output in a JSON file
+	
+	> added my [mahdihasnat](https://github.com/mahdihasnat)
+	
+
+4) ##### **bin_to_dec.py**
+
+	> converts 16-bit floating point decimal in IEEE754 format
+	
+	> added by [NazmulTakbir](https://github.com/NazmulTakbir)
+	
+
